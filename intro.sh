ba3143ef8310fd609f6c4c80779a84b362a844b6
Arnas Udovicius <zordsdavini@gmail.com>
2020-04-18 00:53:49 +0300

    finished intro

--- intro.sh
+++ intro.sh
@@ -20,46 +20,48 @@ sxiv -b logo/qtile_tv-logo-with_background-green.png &
 sleep 1
 sxiv -b logo/qtile_tv-logo-with_background-yellow.png &
 sleep 1
-sxiv -b logo/qtile_tv-logo-with_background-white.png &
-sleep 1
 sxiv -b logo/qtile_tv-logo-with_background-cyan.png &
 sleep 1
 qtile-cmd -o layout -f grow_left
 qtile-cmd -o layout -f grow_left
+qtile-cmd -o layout -f grow_left
+qtile-cmd -o layout -f grow_left
+qtile-cmd -o layout -f grow_left
+qtile-cmd -o layout -f grow_right
+qtile-cmd -o layout -f grow_right
+qtile-cmd -o layout -f grow_down
+qtile-cmd -o layout -f grow_down
+qtile-cmd -o layout -f grow_down
+qtile-cmd -o layout -f grow_down
+qtile-cmd -o layout -f grow_up
+qtile-cmd -o layout -f grow_up
+qtile-cmd -o layout -f grow_up
 sleep 1
 qtile-cmd -o layout -f toggle_split
 sleep 1
 qtile-cmd -o layout -f up
 sleep 1
 qtile-cmd -o layout -f up
+sleep 1
+qtile-cmd -o layout -f toggle_split
 sleep 1
 qtile-cmd -o layout -f shuffle_left
 sleep 1
 qtile-cmd -o layout -f right
 sleep 1
-qtile-cmd -o layout -f toggle_split
-sleep 1
 qtile-cmd -o layout -f shuffle_right
 sleep 1
 
-qtile-cmd -o group 7 -f setlayout -a zoomy
-sleep 1
-qtile-cmd -o layout -f up
-sleep 1
-qtile-cmd -o layout -f up
-sleep 1
-qtile-cmd -o layout -f down
-sleep 1
-
+qtile-cmd -o layout -f normalize
 qtile-cmd -o group 7 -f setlayout -a monadtall
 sleep 1
-qtile-cmd -o layout -f grow
+qtile-cmd -o layout -f maximize
 sleep 1
 qtile-cmd -o layout -f up
 sleep 1
-qtile-cmd -o layout -f grow
+qtile-cmd -o layout -f maximize
 sleep 1
-qtile-cmd -o layout -f shrink
+qtile-cmd -o layout -f maximize
 sleep 1
 qtile-cmd -o layout -f normalize
 sleep 1
@@ -69,8 +71,21 @@ qtile-cmd -o layout -f shuffle_left
 sleep 1
 qtile-cmd -o layout -f flip
 sleep 1
-qtile-cmd -o layout -f kill
+qtile-cmd -o layout -f flip
 sleep 1
+qtile-cmd -o layout -f right
+sleep 1
+qtile-cmd -o layout -f maximize
+sleep 1
+qtile-cmd -o layout -f maximize
+sleep 1
+qtile-cmd -o layout -f left
+qtile-cmd -o layout -f down
+qtile-cmd -o layout -f down
+sleep 1
+
+qtile-cmd -o group 7 -f setlayout -a max
+sleep 3
 
 # stop recording
 pkill -fxn '(/\S+)*ffmpeg\s.*\sx11grab\s.*'
